10d8972fbf4b04ddfddb5ba6a5dd9b3763639c39
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-04-27 16:16:22 +0200

    Refactored Dockerimage to use common base

--- sara_coffee.sh
+++ sara_coffee.sh
@@ -1,13 +1,24 @@
 #!/bin/bash 
 set -x
 
-BINDIR=`pwd -P`
+BINDIR=$(dirname `readlink -f $0`)
 TEMPLATEFILE="$BINDIR/TEMPLATEFILE"
-TARGET=$1
-OUTFILE=$2
+INPUT=$(readlink -f $1)
+OUTFILE=$(readlink -f $2)
+OUTDIR=$(dirname $2)
 
 export PDB_DIR="$BINDIR/PDBdir/"
 export NO_REMOTE_PDB_DIR=1
 unset MAFFT_BINARIES
 
-t_coffee -in $TARGET -method sara_pair -template_file $TEMPLATEFILE,RNA -extend_mode rna2 -outfile $OUTFILE -relax_lib 0 -output clustalw,html  -transform dna2rna -cache=no -n_core 1 -pdb_min_sim 0  
+( cd $OUTDIR; \
+  t_coffee -in $INPUT \
+  -method sara_pair \
+  -template_file $TEMPLATEFILE,RNA -extend_mode rna2 \
+  -outfile $OUTFILE \
+  -relax_lib 0 \
+  -output clustalw,html \
+  -transform dna2rna \
+  -cache=no \
+  -n_core 1 \
+  -pdb_min_sim 0)
